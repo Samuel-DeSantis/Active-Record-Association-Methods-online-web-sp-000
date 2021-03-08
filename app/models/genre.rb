35fe47cd45dc284c3739d56a52991eec29349cd5
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-03-08 04:26:12 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,14 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    self.songs.length
+    self.songs.length #=> Genres.songs=[].size(songs=[])
   end
 
   def artist_count
-    self.artists.length
+    self.artists.length #=> Genres.artists=[].size(artists=[])
   end
 
   def all_artist_names
-    self.artists.all.name
+    self.artists.map(&:name) #=> Genres.artists=[].[]+=artists.name
   end
 end
